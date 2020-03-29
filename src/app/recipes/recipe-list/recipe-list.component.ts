221b1c9273bb8ff33d7bbdf7e34b8526c3069ca6
Julia Fong <Juliamzfong@gmail.com>
2020-03-29 16:01:48 -0500

    Add data with event and property binding in recipe components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -8,13 +8,20 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-    recipes: Recipe[] = [
-      new Recipe('Test Recipe', 'A delicious test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+    @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
+    recipes: Recipe[] = [
+      new Recipe('Test Recipe', 'A delicious test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+      new Recipe('AnotherTest Recipe', 'A delicious test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
     ];
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
+
 }
