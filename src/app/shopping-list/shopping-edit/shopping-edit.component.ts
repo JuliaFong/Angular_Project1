a1d67b9ed23576a064382e6eaef5ce41313341bd
Julia Fong <Juliamzfong@gmail.com>
2020-04-23 14:59:19 -0500

    Add select item function

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Subscription } from 'rxjs/';
 
@@ -10,9 +10,11 @@ import { ShoppingListService } from '../shopping-list.service';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit, OnDestroy {
+  @ViewChild('f', { static: false}) slForm: NgForm
   subscription: Subscription
   editMode = false;
   editedItemIndex: number;
+  editedItem: Ingredient;
    
   constructor(private slService: ShoppingListService) { }
 
@@ -20,8 +22,13 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     this.subscription =this.slService.startedEditing
     .subscribe(
       (index: number) => {
-        this.editedItemIndex = index
+        this.editedItemIndex = index;
         this.editMode = true;
+       this.editedItem = this.slService.getIngredient(index);
+        this.slForm.setValue({
+          name: this.editedItem.name,
+          amount: this.editedItem.amount
+        })
       }
     );
   }
