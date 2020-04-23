daf74fb0d63696fbf6fdb9d7400bb06a838e849f
Julia Fong <Juliamzfong@gmail.com>
2020-04-23 13:52:43 -0500

    Add select items on the list feature

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -2,6 +2,7 @@ import { Ingredient } from '../shared/ingredient.model'
 import { Subject } from 'rxjs';
 export class ShoppingListService {
     ingredientsChanged = new Subject<Ingredient[]>();
+    startedEditing = new Subject<number>();
     private ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
         new Ingredient('Tomatoes', 10),
