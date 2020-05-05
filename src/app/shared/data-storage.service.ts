d799faf3d8fa0b23f940ce078e388f2b3f4f3132
Julia Fong <Juliamzfong@gmail.com>
2020-05-04 23:05:31 -0500

    Clean up response data

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
@@ -22,6 +23,11 @@ export class DataStorageService {
     fetchRecipes() {
         this.http
         .get<Recipe[]>( 'https://recipe-book-80791.firebaseio.com/recipes.json')
+        .pipe(map(recipes => {
+            return recipes.map(recipe => {
+                return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []} 
+            });
+        }))
         .subscribe(recipes => {
            this.recipeService.setRecipes(recipes);
         });
