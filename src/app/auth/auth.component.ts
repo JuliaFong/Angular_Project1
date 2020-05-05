c5908d0ca30cc0bea8ff0427977a79f6c0c35e8e
Julia Fong <Juliamzfong@gmail.com>
2020-05-05 15:30:29 -0500

    Update error handling

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -32,9 +32,8 @@ export class AuthComponent {
             this.authService.signup(email, password).subscribe(resData => {
                 console.log(resData)
                 this.isLoading = false;
-            }, error => {
-                console.log(error)
-                this.error = 'An error has occurred!'
+            }, errorRes => {
+                console.log(errorRes)
                 this.isLoading = false;
             });
         }
