3428d20c65b5189d4c5d69b077938bbacc9d0afc
Julia Fong <Juliamzfong@gmail.com>
2020-05-04 22:55:09 -0500

    Add fetch recipes in data storage

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-
+import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
 
 @Injectable({providedIn: 'root'})
@@ -11,11 +11,19 @@ export class DataStorageService {
     storeRecipes() {
         const recipes = this.recipeService.getRecipes();
         this.http.put(
-            'https://recipe-book-80791.firebaseio.com/recipes.json,', 
+            'https://recipe-book-80791.firebaseio.com/recipes.json', 
         recipes
-        );
+        )
         .subscribe(response => {
             console.log(response)
         })
+    } 
+
+    fetchRecipes() {
+        this.http
+        .get<Recipe[]>( 'https://recipe-book-80791.firebaseio.com/recipes.json')
+        .subscribe(recipes => {
+           this.recipeService.setRecipes(recipes);
+        });
     }
 }
