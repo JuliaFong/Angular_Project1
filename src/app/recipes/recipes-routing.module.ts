bd4856ccfc2b04f81451ab7faad4d0390fea05de
Julia Fong <Juliamzfong@gmail.com>
2020-05-11 20:04:08 -0500

    Add routes to feature module

--- src/app/recipes/recipes-routing.module.ts
+++ src/app/recipes/recipes-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
 import { RecipesComponent } from './recipes.component';
-import { AuthGuard } from './auth/auth.guard';
+import { AuthGuard } from '../auth/auth.guard';
 import { RecipeStartComponent } from './recipe-start/recipe-start.component';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
@@ -23,6 +23,8 @@ const routes: Routes =[
 ]
 
 @NgModule({
+     imports: [RouterModule.forChild(routes)],
+     exports: [RouterModule]
 
 })
 export class RecipesRoutingModule{}
