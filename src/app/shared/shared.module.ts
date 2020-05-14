189adecdf3ab1e5d0a6d41b8b686a2f4c8cd75f6
Julia Fong <Juliamzfong@gmail.com>
2020-05-13 23:26:42 -0500

    Update app module file

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,6 +4,7 @@ import { AlertComponent } from './alert/alert.component';
 import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.component';
 import { PlaceholderDirective } from './placeholder/placeholder.directive';
 import { DropdownDirective } from './dropdown.directive';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
     declarations: [
@@ -11,6 +12,17 @@ import { DropdownDirective } from './dropdown.directive';
         LoadingSpinnerComponent,
         PlaceholderDirective,
         DropdownDirective
-    ]
+    ],
+    imports: [
+        CommonModule
+    ],
+    exports: [
+        AlertComponent,
+        LoadingSpinnerComponent,
+        PlaceholderDirective,
+        DropdownDirective,
+        CommonModule
+    ],
+   entryComponents: [AlertComponent] 
 })
 export class SharedModule {}
