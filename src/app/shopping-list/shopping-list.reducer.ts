b8b89fb4fd60757ef7aa1ee6ad76c647c12f796d
Julia Fong <Juliamzfong@gmail.com>
2020-05-19 17:30:27 -0700

    Update shopping list-reducer file

--- src/app/shopping-list/shopping-list.reducer.ts
+++ src/app/shopping-list/shopping-list.reducer.ts
@@ -1,13 +1,23 @@
+import { Action } from '@ngrx/store';
+
 import { Ingredient } from '../shared/ingredient.model'
 
 const intialState = {
     ingredients: [
         new Ingredient('Apples', 5),
         new Ingredient('Tomatoes', 10),
-        new Ingredient('Potatoes', 8)
+        new Ingredient('Potatoes', 8),
     ]
 }
 
-export function shoppingListReducer(state = intialState, action) {
-    
+export function shoppingListReducer(state = intialState, action: Action) {
+    switch (action.type) {
+     case 'ADD_INGREDIENT': 
+      return {
+          ...state,
+          ingredients: [...state.ingredients, action]
+
+      }
+
+    }
 }
