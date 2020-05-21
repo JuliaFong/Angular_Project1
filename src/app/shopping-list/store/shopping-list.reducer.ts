fc2765658db00628123a61c47972e8b04be87640
Julia Fong <Juliamzfong@gmail.com>
2020-05-21 14:48:27 -0700

    Update recipe service file actions

--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -1,3 +1,5 @@
+
+
 import { Ingredient } from '../../shared/ingredient.model';
 import * as ShoppingListActions from './shopping-list.actions';
 
@@ -7,7 +9,7 @@ const initialState = {
 
 export function shoppingListReducer(
   state = initialState,
-  action: ShoppingListActions.AddIngredient
+  action: ShoppingListActions.ShoppingListActions
 ) {
   switch (action.type) {
     case ShoppingListActions.ADD_INGREDIENT:
@@ -15,6 +17,11 @@ export function shoppingListReducer(
         ...state,
         ingredients: [...state.ingredients, action.payload]
       };
+      case ShoppingListActions.ADD_INGREDIENTS:
+          return {
+              ...state,
+              ingredients: [...state.ingredients, ...action.payload]
+          }
     default:
       return state;
   }
